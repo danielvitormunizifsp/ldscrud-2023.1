450a94af858d5ea1e0bfff91b3657651cbd95826
danielvitormunizifsp <108248877+danielvitormunizifsp@users.noreply.github.com>
2023-02-25 21:11:38 -0300

    changes na criação das tabelas
    changes na criação das tabelas e configuração do banco mysql

--- petshop.sql
+++ petshop.sql
@@ -27,141 +27,42 @@ SET time_zone = "+00:00";
 -- Estrutura para tabela `cliente`
 --
 
-CREATE TABLE `cliente` (
+CREATE TABLE `tarefa` (
   `id` int(11) NOT NULL,
-  `nome` varchar(50) NOT NULL,
-  `sobrenome` varchar(50) NOT NULL,
-  `telefone` varchar(50) NOT NULL,
-  `nascimento` varchar(50) NOT NULL,
-  `nomepet` varchar(50) NOT NULL
+  `resumo da tarefa` varchar(50) NOT NULL,
+  `descricao` varchar(50) NOT NULL,
+  `responsavel` varchar(50) NOT NULL,
+  `data da tarefa` varchar(50) NOT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Despejando dados para a tabela `cliente`
 --
 
-INSERT INTO `cliente` (`id`, `nome`, `sobrenome`, `telefone`, `nascimento`, `nomepet`) VALUES
-(1, 'Leandro', 'Artilha', '11978404237', '03-10-1999', 'Lily');
+INSERT INTO `tarefa` (`id`, `resumo`, `descricao`, `responsavel`, `data da tarefa`) VALUES
+(1, 'criar crud', 'realizar a criação de um crud', 'danielcorreia', '20230101');
 
 -- --------------------------------------------------------
 
---
--- Estrutura para tabela `loginUser`
---
 
-CREATE TABLE `loginUser` (
-  `id` int(11) NOT NULL,
-  `loginUser` varchar(30) NOT NULL,
-  `senha` varchar(40) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Despejando dados para a tabela `loginUser`
---
-
-INSERT INTO `loginUser` (`id`, `loginUser`, `senha`) VALUES
-(1, 'teste', '123'),
-(2, 'testando', '1234');
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `pet`
---
-
-CREATE TABLE `pet` (
-  `id` int(50) NOT NULL,
-  `nome` varchar(50) NOT NULL,
-  `idade` varchar(50) NOT NULL,
-  `raça` varchar(50) NOT NULL,
-  `nomedono` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Despejando dados para a tabela `pet`
---
-
-INSERT INTO `pet` (`id`, `nome`, `idade`, `raça`, `nomedono`) VALUES
-(3, 'Lily', '3', 'Pastor Alemão', 'Leandro Artilha'),
-(4, 'Dani', '5', 'Labrador', 'Leandro Artilha'),
-(5, 'Petinho', '12', 'Gato Pintado', 'Ursula');
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `servico`
---
-
-CREATE TABLE `servico` (
-  `id` int(11) NOT NULL,
-  `servico` varchar(50) NOT NULL,
-  `preco` varchar(50) NOT NULL,
-  `agendamento` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Despejando dados para a tabela `servico`
---
-
-INSERT INTO `servico` (`id`, `servico`, `preco`, `agendamento`) VALUES
-(4, 'Tosa', '50,00', '10-10-2022');
-
---
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices de tabela `cliente`
+-- Índices de tabela `tarefa`
 --
-ALTER TABLE `cliente`
+ALTER TABLE `tarefa`
   ADD PRIMARY KEY (`id`);
 
+-
 --
--- Índices de tabela `loginUser`
+-- AUTO_INCREMENT de tabela `tarefa`
 --
-ALTER TABLE `loginUser`
-  ADD PRIMARY KEY (`id`);
-
---
--- Índices de tabela `pet`
---
-ALTER TABLE `pet`
-  ADD PRIMARY KEY (`id`);
-
---
--- Índices de tabela `servico`
---
-ALTER TABLE `servico`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT para tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `cliente`
---
-ALTER TABLE `cliente`
+ALTER TABLE `tarefa`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT de tabela `loginUser`
---
-ALTER TABLE `loginUser`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 
---
--- AUTO_INCREMENT de tabela `pet`
---
-ALTER TABLE `pet`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT de tabela `servico`
---
-ALTER TABLE `servico`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
